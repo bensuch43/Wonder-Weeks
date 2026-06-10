58fedeb95142c1792f0537e3d8de8c7700cf249f
bensuch43 <bensuch43@gmail.com>
2026-06-10 18:03:59 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Wonder Weeks — Service Worker v2
-const CACHE_NAME = 'wonder-weeks-v2';
+// Wonder Weeks — Service Worker v4
+const CACHE_NAME = 'wonder-weeks-v4';
 const ASSETS = [
   '/Wonder-Weeks/',
   '/Wonder-Weeks/index.html',
